95885f0984bbb2885a41a22c73ed3cbadc8ce4b2
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-04-30 22:52:02 +0200

    Add a script to enable black burger button bars on a black background

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,33 @@
 let nav;
 let burgerBtn;
 let allNavItems;
+let burgerBtnBars;
+let allSections;
+let footerYear;
 
 const main = () => {
   prepareElements();
   addEventListeners();
+  handleCurrentYear();
 }
 
 const prepareElements = () => {
   nav = document.querySelector(".nav");
   burgerBtn = document.querySelector(".burger-btn");
-  allNavItems = document.querySelectorAll(".nav__item")
+  allNavItems = document.querySelectorAll(".nav__item");
+  burgerBtnBars = document.querySelector(".burger-btn__bars");
+  allSections = document.querySelectorAll(".section");
+  footerYear = document.querySelector(".footer__year");
 }
 
 const addEventListeners = () => {
-  burgerBtn.addEventListener("click", toggleNavbar);  
+  burgerBtn.addEventListener("click", toggleNavbar);
+  window.addEventListener("scroll", handleObserver);
 }
 
 const toggleNavbar = () => {
   nav.classList.toggle("nav--active");
+  burgerBtnBars.classList.remove("black-bars");
   
   allNavItems.forEach((navItem) => {
     navItem.addEventListener("click", () => {
@@ -27,6 +36,7 @@ const toggleNavbar = () => {
   });
   
   handleNavItemAnimation();
+  handleObserver();
 }
 
 const handleNavItemAnimation = () => {
@@ -36,6 +46,23 @@ const handleNavItemAnimation = () => {
     navItem.classList.toggle("nav__item-animation");
     navItem.style.animationDelay = `${delay}ms`;
     delay += 100;
+  });
+}
+
+const handleCurrentYear = () => {
+  const year = new Date().getFullYear();
+  footerYear.textContent = year;
+}
+
+const handleObserver = () => {
+  const currentSection = window.scrollY; // Położenie krawędzi przeglądarki
+
+  allSections.forEach((section) => {
+    if (section.classList.contains("white-section") && section.offsetTop <= currentSection + 20) {
+      burgerBtnBars.classList.add("black-bars");
+    } else if (!section.classList.contains("white-section") && section.offsetTop <= currentSection + 20) {
+      burgerBtnBars.classList.remove("black-bars");
+    }
   });
 }
 
