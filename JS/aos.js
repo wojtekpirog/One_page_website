f18524d8ffc35a7c938960b7d882dfb6da1d021b
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-05-31 13:53:27 +0200

    Apply on-scroll animation effects

---
+++ JS/aos.js
@@ -0,0 +1,10 @@
+AOS.init({
+  // Global settings:
+  disable: false,
+  startEvent: "DOMContentLoaded",
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 200,
+  delay: 0,
+  duration: 1000,
+  once: true,
+});
