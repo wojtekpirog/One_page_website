17a9670bb32b388de18c533ea551844c4caf6d15
Wojciech Piróg <wojtekpirog12@interia.pl>
2025-08-30 20:57:30 +0200

    Move fonts and icons to public assets

--- gulpfile.js
+++ gulpfile.js
@@ -17,3 +17,127 @@ const sourcemaps = require("gulp-sourcemaps");
 // BrowserSync
 const browserSync = require("browser-sync").create();
 const reload = browserSync.reload;
+// Gulp-if
+const gulpIf = require("gulp-if");
+// Path
+const path = require("path");
+
+const paths = {
+  styles: {
+    src: "./src/SCSS/**/*.scss",
+    dist: "./public/css",
+  },
+
+  scripts: {
+    src: "./src/JS/script.js",
+    dist: "./public/JS"
+  },
+
+  images: {
+    src: "./src/assets/images/**/*",
+    dist: "./public/assets/images"
+  },
+
+  html: "./public/index.html",
+  dist: "./public"
+};
+
+function prepareCSS() {
+  return src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", (error) => {
+      console.error(`Sass error: ${error}`)
+    }))
+    .pipe(postcss([
+      autoprefixer({
+        cascade: false,
+        grid: "autoplace"
+      }),
+      cssnano({
+        preset: ["default", {
+          discardComments: {
+            removeAll: true
+          }
+        }]
+      })
+    ]))
+    .pipe(rename({
+      basename: "style",
+      suffix: ".min",
+      extname: ".css"
+    })).on("error", (error) => {
+      console.error(`Rename error: ${error}`)
+    })
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(paths.styles.dist));
+}
+
+function transformJS() {
+  return src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ["@babel/env"],
+    }).on("error", (error) => {
+      console.error(`Babel error: ${error}`);
+    }))
+    .pipe(terser({
+      toplevel: true
+    }))
+    .pipe(rename({
+      basename: "script",
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(paths.scripts.dist));
+}
+
+function isNotWebp(file) {
+  return path.extname(file.path).toLowerCase() !== ".webp";
+}
+
+function compressImages() {
+  return src(paths.images.src)
+    .pipe(gulpIf(
+      isNotWebp, 
+      imagemin().on("error", (error) => {
+        console.error(`Imagemin error: ${error}`)
+      })
+    ))
+    .pipe(gulpIf(
+      isNotWebp, 
+      rename({suffix: ".min"}).on("error", (error) => {
+        console.error(`Rename error: ${error}`)
+      })
+    ))
+    .pipe(dest(paths.images.dist));
+}
+
+function startBrowserSync(cb) {
+  browserSync.init(
+    {
+      host: "192.168.1.101",
+      port: 3000,
+      injectChanges: true,
+      server: {
+        baseDir: "./public",
+        index: "index.html"
+      },
+    }
+  );
+
+  cb();
+}
+
+function watchForChanges() {
+  watch(paths.html).on("change", reload);
+  watch(paths.styles.src, prepareCSS).on("change", reload);
+  watch(paths.scripts.src, transformJS).on("change", reload);
+  watch(paths.images.src, compressImages).on("change", reload);
+}
+
+module.exports.default = series(prepareCSS, transformJS, compressImages, startBrowserSync, watchForChanges);
+module.exports.prepareCSS = prepareCSS;
+module.exports.compressImages = compressImages;
+module.exports.startBrowserSync = startBrowserSync;
+module.exports.watch = watch;
